117da0efff36c71aaff005db83b39aa333f8a375
gjgetzinger <gordon.getzinger@duke.edu>
2020-03-27 12:12:51 -0400

    update docs

---
+++ R/cfm-id-precomputed.R
@@ -0,0 +1,42 @@
+#' CFM-ID Precomupted
+#'
+#' @param spectrum_file The filename where the input spectra can be found. This
+#'   can be a .msp file in which the desired spectrum is listed under a
+#'   corresponding id (next arg). Or it could be a single file with a list of
+#'   peaks 'mass intensity' delimited by lines, with either 'low','med' and
+#'   'high' lines beginning spectra of different energy levels, or 'energy0',
+#'   'energy1', etc.
+#' @param id An identifier for the target molecule (printed to output, in case
+#'   of multiple concatenated results, and used to retrieve input spectrum from
+#'   msp where msp is used).
+#' @param candidate_file The filename where the input list of candidate
+#'   structures can be found - line separated 'id smiles_or_inchi spectrum_file'
+#'   triples, where the spectrum file stores the precomputed spectra (no spaces
+#'   allowed).
+#' @param num_highest The number of (ranked) candidates to return or -1 for all
+#'   (if not given, returns all in ranked order)
+#' @param ppm_mass_tol The mass tolerance in ppm to use when matching peaks
+#'   within the dot product comparison - will use higher resulting tolerance of
+#'   ppm and abs (if not given defaults to 10ppm)
+#' @param abs_mass_tol The mass tolerance in abs Da to use when matching peaks
+#'   within the dot product comparison - will use higher resulting tolerance of
+#'   ppm and abs ( if not given defaults to 0.01Da)
+#' @param score_type The type of scoring function to use when comparing spectra.
+#'   Options: Jaccard (default), DotProduct
+#' @param output_filename The filename of the output file to write to (if not
+#'   given, prints to stdout)
+#'
+#' @return
+#' @export
+#'
+cfm_id_precomputed <-
+  function(spectrum_file,
+           id,
+           candidate_file,
+           num_highest,
+           ppm_mass_tol,
+           abs_mass_tol,
+           score_type,
+           output_filename) {
+
+  }
---
+++ R/cfm-predict.R
@@ -0,0 +1,215 @@
+#' CFM-predict
+#'
+#' @param input_smiles_or_inchi The smiles or inchi string of the structure
+#'   whose spectra you want to predict.
+#' @param prob_thresh_for_prune The probability below which to prune unlikely
+#'   fragmentations (default 0.001)
+#' @param param_filename The filename where the parameters of a trained cfm
+#'   model can be found (if not given, assumes param_output.log in current
+#'   directory)
+#' @param config_filename The filename where the configuration parameters of the
+#'   cfm model can be found (if not given, assumes param_config.txt in current
+#'   directory)
+#' @param include_annotations Whether to include fragment information in the
+#'   output spectra (0 = NO (default), 1 = YES ). Note: ignored for msp/mgf
+#'   output.
+#' @param output_filename The filename of the output spectra file to write to
+#'   (if not given, returns list from stdout).
+#' @param apply_post_processing Whether or not to post-process predicted spectra
+#'   to take the top 80% of energy (at least 5 peaks), or the highest 30 peaks
+#'   (whichever comes first) (0 = OFF, 1 = ON (default) ).
+#'
+#' @return When a output filename is provided, the path to the file. If no
+#'   filename is provided, results are extracted from the standard output into a
+#'   named list.
+#' @export
+#'
+cfm_predict <-
+  function(input_smiles_or_inchi,
+           prob_thresh_for_prune = 0.001,
+           param_filename,
+           config_filename,
+           include_annotations = 0,
+           output_filename = NULL,
+           apply_post_processing = 1) {
+
+    args <- paste(
+      paste0("'", input_smiles_or_inchi, "'"),
+      prob_thresh_for_prune,
+      param_filename,
+      config_filename,
+      include_annotations
+    )
+
+    if (!is.null(output_filename)) {
+      args <-
+        paste(args, output_filename, apply_post_processing)
+    }
+
+    rst <-
+      system2(
+        command = 'cfm-predict',
+        args = args,
+        stdout = is.null(output_filename),
+        wait = T
+      )
+    if (is.null(output_filename)) {
+      out <- cfm_predict_stdout(rst)
+      return(out)
+    } else {
+       return(output_filename)
+     }
+  }
+
+#' Read cfm-predict standard output into R object
+#'
+#' @param rst Standard out from a completed system2 call to cfm-predict
+#'
+#' @return List containing predicted spectra (and annotations, if present)
+#' @export
+#'
+cfm_predict_stdout <- function(rst){
+  rst_break <- which(sapply(rst, nchar) == 0)
+  spec <- rst[1:(rst_break-1)]
+  spec_f <- cumsum(as.numeric(grepl('energy', spec)))
+  spec_breaks <- !grepl('energy', spec) & spec != ""
+  spec_split <- setNames(
+    split(spec[spec_breaks], spec_f[spec_breaks]),
+    spec[grepl('energy', spec)])
+
+  spec_out <- lapply(spec_split, function(x){
+    do.call('rbind', lapply(x, function(xx) {
+      y <- strsplit(xx, ' ')
+      data.frame(matrix(
+        c(y[[1]][1:2]),
+        ncol = 2,
+        byrow = T,
+        dimnames = list(c(), c('mz', 'int'))
+      ),
+      stringsAsFactors = F)
+    }))
+  })
+
+  if (rst_break == length(rst)) {
+    out <- spec_out
+  } else {
+    spec_ann <- lapply(spec_split, function(x) {
+      sapply(strsplit(x, ' '), function(xx) {
+        paste0(xx[3:length(xx)], collapse = ' ')
+      })
+    })
+    annotation <-
+      data.frame(matrix(do.call('rbind', strsplit(rst[(rst_break + 1):length(rst)], ' ')),
+             ncol = 3,
+             dimnames = list(c(), c('idx', 'mz', 'smiles'))), stringsAsFactors = F)
+    out <-
+      list(
+        spec = spec_out,
+        spec_ann = spec_ann,
+        annotation = annotation
+      )
+  }
+  return(out)
+}
+
+#' Read cfm_predict results from output_filename
+#'
+#' @param output_filename
+#'
+#' @return List containing predicted spectra (and annotations, if present)
+#' @export
+#'
+cfm_predict_readfile <- function(output_filename) {
+  rst <-
+    read.delim(
+      file = output_filename,
+      header = F,
+      check.names = F,
+      stringsAsFactors = F
+    )[, 1]
+  rst <- append(rst, "", after = which(grepl('^0', rst)) - 1)
+  cfm_predict_stdout(rst = rst)
+}
+
+#' Batch CFM prediction using a Linux cluster with a SLURM scheduler
+#'
+#' @param id A molecule identifier. Used for the filename of the cfm-predict
+#'   output file.
+#' @param out_dir A directory to write the cfm-predict results.
+#' @param slurm_options A named list of options recognized by sbatch; see
+#'   Details below for more information.
+#' @param cpus_per_node The number of CPUs per node on the cluster; determines
+#'   how many processes are run in parallel per node.
+#' @param nodes The (maximum) number of cluster nodes to spread the calculation
+#'   over. slurm_apply automatically divides params in chunks of approximately
+#'   equal size to send to each node. Less nodes are allocated if the parameter
+#'   set is too small to use all CPUs on the requested nodes.
+#' @param input_smiles_or_inchi The smiles or inchi string of the structure
+#'   whose spectra you want to predict.
+#' @param prob_thresh_for_prune The probability below which to prune unlikely
+#'   fragmentations (default 0.001)
+#' @param param_filename The filename where the parameters of a trained cfm
+#'   model can be found (if not given, assumes param_output.log in current
+#'   directory)
+#' @param config_filename The filename where the configuration parameters of the
+#'   cfm model can be found (if not given, assumes param_config.txt in current
+#'   directory)
+#' @param include_annotations Whether to include fragment information in the
+#'   output spectra (0 = NO (default), 1 = YES ). Note: ignored for msp/mgf
+#'   output.
+#' @param output_filename The filename of the output spectra file to write to
+#'   (if not given, returns list from stdout).
+#' @param apply_post_processing Whether or not to post-process predicted spectra
+#'   to take the top 80% of energy (at least 5 peaks), or the highest 30 peaks
+#'   (whichever comes first) (0 = OFF, 1 = ON (default) ).
+#'
+#' @return A slurm_job object containing the jobname and the number of nodes
+#'   effectively used.
+#' @export
+#'
+cfm_predict_batch <-
+  function(id,
+           out_dir,
+           input_smiles_or_inchi,
+           prob_thresh_for_prune = 0.001,
+           include_annotations = 1,
+           apply_post_processing = 1,
+           param_filename,
+           config_filename,
+           slurm_options,
+           cpus_per_node,
+           nodes) {
+    sjob <- rslurm::slurm_apply(
+      f = function(input_smiles_or_inchi,
+                   output_filename) {
+        cfm_predict(
+          input_smiles_or_inchi = input_smiles_or_inchi,
+          prob_thresh_for_prune = prob_thresh_for_prune,
+          param_filename = param_filename,
+          config_filename = config_filename,
+          include_annotations = include_annotations,
+          output_filename = output_filename,
+          apply_post_processing = apply_post_processing
+        )
+      },
+      params = data.frame(
+        input_smiles_or_inchi = input_smiles_or_inchi,
+        output_filename = paste0(out_dir, '/', id, '.txt')
+      ),
+      add_objects = c(
+        'param_filename',
+        'config_filename',
+        'cfm_predict',
+        'prob_thresh_for_prune',
+        'input_smiles_or_inchi',
+        'include_annotations',
+        'apply_post_processing'
+      ),
+      nodes = nodes,
+      cpus_per_node = cpus_per_node,
+      slurm_options = slurm_options,
+      submit = T
+    )
+    return(sjob)
+  }
+
