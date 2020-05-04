b250c5273a6c7936a79c9d583038a94b01d243aa
gjgetzinger <gordon.getzinger@duke.edu>
2020-05-03 20:47:32 -0400

    add option to search by molform

--- R/cfm-search.R
+++ R/cfm-search.R
@@ -14,11 +14,14 @@
 #' @param molprop_table_name The name of the table continaing molecular properties
 #' @param pol Polarity (positive or negative)
 #' @param ppm Mass error for filtering molecules by neutral mass
+#' @param excl_precur Should precursor ion from query spectrum be excluded. (T/F)
+#' @param mol_form The molecular formula assigned to the query spectrum.
 #'
 #' @return A tibble with variables ID, %fun_similarity
 #' @export
 #'
 cfm_search <- function(query_spectrum,
+                       mol_form = NULL,
                        db_file = NULL,
                        spec_table_name = NULL,
                        mol_table_name = NULL,
@@ -27,7 +30,9 @@ cfm_search <- function(query_spectrum,
                        fun = c("common", "cor", "dotproduct"),
                        bin = 1,
                        pol = c('negative', 'positive'),
-                       ppm = 10) {
+                       ppm = 10,
+                       excl_precur = F
+                       ) {
   stopifnot(class(query_spectrum) == 'Spectrum2')
   pol <- match.arg(pol, several.ok = F)
   fun <- match.arg(fun, several.ok = F)
@@ -43,27 +48,45 @@ cfm_search <- function(query_spectrum,
                 ID = ID)
   if(is.null(ID)){
     # get IDs within by precur mz
-    neutral_mass <- dplyr::filter(
-      adducts,
-      charge == switch(pol, negative = -1, positive = 1)*query_spectrum@precursorCharge
-    ) %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(
-        neu_mass = (query_spectrum@precursorMz + charge*mass)*mult
+    if(!is.null(mol_form)){ # prefer formula query to mass search
+      if(grepl('+', mol_form)){
+        alt_molform <- cdutils::clean_form(molform = mol_form, as_cts = T)
+        alt_molform['H'] <- alt_molform['H'] - 1
+        alt_molform[alt_molform == 1] <- ""
+        alt_molform <- paste0(names(alt_molform), alt_molform, collapse = '')
+        mol_form <- append(mol_form, alt_molform)
+      }
+
+     mol_props <- mol_props %>%
+        filter(MolForm %in% mol_form)
+
+    } else {
+      neutral_mass <- dplyr::filter(
+        adducts,
+        charge == switch(pol, negative = -1, positive = 1)*query_spectrum@precursorCharge
       ) %>%
-      dplyr::ungroup() %>%
-      dplyr::transmute(adduct, mass = neu_mass)
+        dplyr::rowwise() %>%
+        dplyr::mutate(
+          neu_mass = (query_spectrum@precursorMz + charge*mass)*mult
+        ) %>%
+        dplyr::ungroup() %>%
+        dplyr::transmute(adduct, mass = neu_mass)
 
-    mol_props <- dplyr::filter(mol_props,
-                        purrr::map_lgl(
-                          mol_props$ExactMass,
-                          .f = function(x) {
-                            any(abs(1e6 * ((
-                              neutral_mass$mass - x
-                            ) / x)) < (ppm / 2))
-                          }
-                        ))
+      mol_props <- dplyr::filter(mol_props,
+                                 purrr::map_lgl(
+                                   mol_props$ExactMass,
+                                   .f = function(x) {
+                                     any(abs(1e6 * ((
+                                       neutral_mass$mass - x
+                                     ) / x)) < ppm / 2)
+                                   }
+                                 ))
+    }
 
+    if(nrow(mol_props) == 0){
+      message('No molecules matched')
+      return(NULL)
+    }
     ## filter mols
     mol_dat <- dplyr::left_join(mol_props, mols, by = 'ID')
     ID <- dplyr::pull(mol_dat, ID)
@@ -84,10 +107,21 @@ cfm_search <- function(query_spectrum,
   # get spec similarity
   sim_score <- purrr::map_dbl(
     s2,
-    .f = MSnbase::compareSpectra,
-    object1 = query_spectrum,
-    fun = fun,
-    bin = bin
+    .f = function(x){
+      if(excl_precur){
+        MSnbase::compareSpectra(
+          object1 = filterMz(query_spectrum, c(min(mz(query_spectrum)), query_spectrum@precursorMz - bin)),
+          object2 = filterMz(x, c(min(mz(x)), query_spectrum@precursorMz - bin)),
+          fun = fun,
+          bin = bin)
+      } else {
+        MSnbase::compareSpectra(
+          object1 = query_spectrum,
+          object2 = x,
+          fun = fun,
+          bin = bin)
+      }
+    }
   ) %>%
     tibble::enframe(name = 'ID') %>%
     dplyr::arrange(desc(value)) %>%
